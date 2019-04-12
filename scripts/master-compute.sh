fd4a2c5069147b35599c3947f186125bd91a533b
bobfogg <bob.fogg@celltracktech.com>
2019-04-12 19:04:29 -0400

    debug

--- scripts/master-compute.sh
+++ scripts/master-compute.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 export NODE_ENV=production VAMP_PATH=/home/pi/.vamp NODE_PATH=/usr/local/lib/node_modules LC_ALL="C.UTF-8"
-/usr/bin/node /home/pi/proj/sensorgnome/master/master.js > /var/log/nodelog.txt 2>&1
+/usr/bin/node /home/pi/proj/sensorgnome/master/master.js
