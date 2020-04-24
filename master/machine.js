62560db2eefe1d53d0605325dd95862e230ed926
bobfogg <bob.fogg@celltracktech.com>
2020-04-24 12:04:38 -0400

    ensure station id is stripped

--- master/machine.js
+++ master/machine.js
@@ -13,7 +13,7 @@
 */
 
 //var machineID = Fs.readFileSync("/etc/beaglebone_id").toString().substring(0, 12);
-var machineID = "CTT-"+(Fs.readFileSync("/etc/ctt/station-id").toString());
+var machineID = "CTT-"+(Fs.readFileSync("/etc/ctt/station-id").toString().trim());
 var bootCountFile = "/etc/bootcount"
 var bootCount = "" + (Fs.existsSync(bootCountFile) ?
                       Number(Fs.readFileSync(bootCountFile).toString()) % (1000000)
