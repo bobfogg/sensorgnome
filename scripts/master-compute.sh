2aa893506cee22e0342b1347f2b44dacdd64bc7d
Bob Fogg <bob.fogg@celltracktech.com>
2022-04-19 14:48:01 -0400

    update username directory references to ctt for bullseye build

--- scripts/master-compute.sh
+++ scripts/master-compute.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-export NODE_ENV=production VAMP_PATH=/home/pi/.vamp NODE_PATH=/usr/local/lib/node_modules LC_ALL="C.UTF-8"
-/usr/bin/node /home/pi/proj/sensorgnome/master/master.js
+export NODE_ENV=production VAMP_PATH=/home/ctt/.vamp NODE_PATH=/usr/local/lib/node_modules LC_ALL="C.UTF-8"
+/usr/bin/node /home/ctt/proj/sensorgnome/master/master.js
